980e31f065660f5ee76c0f2e591611bdef8236c8
mposso7 <micposso@gmail.com>
2016-10-18 11:40:47 -0400

    added form module

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { ProfileComponent }  from './components/profile.component';
 import { HttpModule } from '@angular/http';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
-  imports: [ BrowserModule, HttpModule ],
+  imports: [ BrowserModule, HttpModule, FormsModule ],
   declarations: [ AppComponent, ProfileComponent ],
   bootstrap: [ AppComponent ]
 })
