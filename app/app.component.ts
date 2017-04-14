f06cc804ca3b393e2a28b511f5f25bb910397e89
micposso <mposso@babbel.com>
2017-04-14 04:06:01 +0200

    madify layout

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,28 +3,7 @@ import {GithubService} from './services/github.service';
 
 @Component({
     selector: 'my-app',
-    template: `
-    <nav class="navbar navbar-default">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">GitHug</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
-    <div class="container"><profile></profile></div>
-  `
-  ,
+    template: `<div class="container"><profile></profile></div>`,
     providers: [GithubService]
 })
 
